a103fa262311c3407a7be73ae50cc3df7980b0c3
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-20 12:06:03 +0530

    [Tejas Tonpe] Add: created python program which calculates percentage of head & tail of coins

---
+++ flip_coin.py
@@ -0,0 +1,32 @@
+
+import random
+
+def Flip_Coin(no_coin_fliped):
+    if no_coin_fliped <=0:
+        print("Enter valid no of times coin flipped")
+        
+
+    
+    head_count=0
+    tail_count=0
+
+    for flip in range(no_coin_fliped):
+        value=random.random()
+        if value < 0.5:
+            tail_count +=1
+        else:
+            head_count +=1
+    
+    print(f"Percentage of Head: {(head_count/no_coin_fliped)*100} %")
+    print(f"Percentage of Tail: {(tail_count/no_coin_fliped)*100} %")
+    
+
+if __name__=="__main__":
+    # Flip_Coin(110)
+    try:
+        num_flips = int(input("Enter the number of times to flip the coin: "))
+        Flip_Coin(num_flips)
+    except ValueError:
+        print("Please enter a valid positive integer.")
+
+
