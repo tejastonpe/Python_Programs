11d48c62eba632f0ae3edb20f6366a2c5cca3045
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-20 12:19:38 +0530

    [Tejas Tonpe] Add: created python function which takes user input & replace with string template

--- user_input.py
+++ user_input.py
@@ -1,12 +1,10 @@
-def User_Input():
+def user_input():
     name = str(input("Enter your name: "))
     if len(name) < 3:
-        print("Enter valid name hanving more than three characters")
+        print("Enter valid name hanving three characters")
     else:
         print(f"Hello {name}, How are you?")
+        
     
-
-
-
 if __name__=="__main__":
-    User_Input()
+    user_input()
