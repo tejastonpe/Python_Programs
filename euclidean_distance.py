4fdf84784f70c41139167f6c62b86ac7c30d0b89
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-22 10:55:44 +0530

    [Tejas Tonpe] Add: created python program which finds euclidean distance between points to origin

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -4,13 +4,17 @@ def find_distance(x,y):
     dist=math.sqrt(x*x + y*y)
     print(f"Euclidean Distance from points {x,y} & (0,0) is {dist} ")
  
-
-if __name__=="__main__":
+def main():
     try:
         x=int(input("Enter x co-ordinate: "))
         y=int(input("Enter y co-ordinate: "))        
        
         find_distance(x,y)
     except ValueError:
-        print("Enter integer values")
+        print("Enter integer values")    
+
+
+if __name__=="__main__":
+    main()
+
     
